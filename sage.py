c2459215c6e6b264beaa57b0316c6500dfc3f60e
RAJAT <rajat1234chauhan@gmail.com>
2023-04-30 03:08:13 +0530

    completed script

--- sage.py
+++ sage.py
@@ -14,8 +14,12 @@ urls={
     "sendMessage": f"https://api.telegram.org/bot{TOKEN}/sendMessage",
     "sendPhoto" : f"https://api.telegram.org/bot{TOKEN}/sendPhoto",
     "sendAnimation" : f"https://api.telegram.org/bot{TOKEN}/sendAnimation",
-    "sendPoll" : f"https://api.telegram.org/bot{TOKEN}/sendPoll"
+    "sendPoll" : f"https://api.telegram.org/bot{TOKEN}/sendPoll",
+    "sendDocument" : f'https://api.telegram.org/bot{TOKEN}/sendDocument'
 }
+
+def tel_send_message(chat_id, text):
+    requests.post(urls['sendMessage'], json={'chat_id': chat_id,'text': text})
 
 def tel_add_anime(chat_id, text):
     text = list(text.split('\n'))
@@ -111,18 +115,69 @@ def parse_poll(message):
 
     return poll_id, user, option
  
+def tel_parse_get_message(message):
+    print("message-->", message)
+
+    try:  # if the file is an image
+        g_chat_id = message['message']['chat']['id']
+        g_file_id = message['message']['photo'][0]['file_id']
+        print("g_chat_id-->", g_chat_id)
+        print("g_image_id-->", g_file_id)
+
+        return g_file_id
+    except:
+        try:  # if the file is a video
+            g_chat_id = message['message']['chat']['id']
+            g_file_id = message['message']['video']['file_id']
+            print("g_chat_id-->", g_chat_id)
+            print("g_video_id-->", g_file_id)
+
+            return g_file_id
+        except:
+            try:  # if the file is an audio
+                g_chat_id = message['message']['chat']['id']
+                g_file_id = message['message']['audio']['file_id']
+                print("g_chat_id-->", g_chat_id)
+                print("g_audio_id-->", g_file_id)
+
+                return g_file_id
+            except:
+                try:  # if the file is a document
+                    g_chat_id = message['message']['chat']['id']
+                    g_file_id = message['message']['document']['file_id']
+                    print("g_chat_id-->", g_chat_id)
+                    print("g_file_id-->", g_file_id)
+
+                    return g_file_id
+                except:
+                    pass
+
 def parse_message(message):
-    print("message-->",message)
-    chat_id = message['message']['chat']['id']
-    txt = message['message']['text']
-    date = message['message']['date']
-    import datetime, time
-    dftime = time.mktime(datetime.datetime.now().timetuple())
-    if int(dftime-10) > date:
-        return chat_id, ""
-    print("chat_id-->", chat_id)
-    print("txt-->", txt)
-    return chat_id,txt
+    try:
+        print("message-->",message)
+        date = message['message']['date']
+        import datetime, time
+        dftime = time.mktime(datetime.datetime.now().timetuple())
+        if int(dftime-10) > date:
+            return chat_id, ""
+        chat_id = message['message']['chat']['id']
+        txt = message['message']['text']
+        print("chat_id-->", chat_id)
+        print("txt-->", txt)
+
+        return chat_id, txt
+    except:
+        print("NO text found-->>")
+
+    try:
+        cha_id = message['callback_query']['from']['id']
+        i_txt = message['callback_query']['data']
+        print("cha_id-->", cha_id)
+        print("i_txt-->", i_txt)
+
+        return cha_id, i_txt
+    except:
+        pass
 
 def tel_toss(chat_id):
     tails="https://qph.cf2.quoracdn.net/main-qimg-148ae81e6fe0500e130fb547026a9b26-lq"
@@ -168,6 +223,7 @@ def tel_update_score(chat_id, text, msg):
             json.dump(data, file)
         file.close()
         requests.post(urls['sendMessage'], json={'chat_id': chat_id,'text': f'Updated {text[1]}\'s score to {text[2]} issued by @{msg["message"]["from"]["username"]}'})
+        tel_show_score(chat_id)
     else:
         requests.post(urls['sendMessage'],json={'chat_id':chat_id,'text':'Enter username with score please :)'})
 
@@ -208,6 +264,83 @@ def tel_help(chat_id):
    
     requests.post(urls['sendMessage'],json=payload)
 
+def tel_upload_file(file_id):
+    # Getting the url for the file
+    url = f'https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}'
+    a = requests.post(url)
+    json_resp = json.loads(a.content)
+    print("json_resp-->", json_resp)
+    file_path = json_resp['result']['file_path']
+    print("file_path-->", file_path)
+
+    # saving the file to our computer
+    url_1 = f'https://api.telegram.org/file/bot{TOKEN}/{file_path}'
+    b = requests.get(url_1)
+    file_content = b.content
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+
+def tel_send_document(chat_id, text):
+    payload = {
+        'chat_id': chat_id,
+        "document": "http://www.africau.edu/images/default/sample.pdf",
+    }
+    requests.post(urls['sendDocument'], json=payload)
+
+def tel_send_inlinebutton(chat_id):
+    payload = {
+        'chat_id': chat_id,
+        'text': "What is this?",
+        'reply_markup': {
+            "inline_keyboard": [[
+                {
+                    "text": "A",
+                    "callback_data": "ic_A"
+                },
+                {
+                    "text": "B",
+                    "callback_data": "ic_B"
+                }]
+            ]
+        }
+    }
+    requests.post(urls['sendMessage'], json=payload)
+
+def tel_send_inlineurl(chat_id):
+    payload = {
+        'chat_id': chat_id,
+        'text': "Which link would you like to visit?",
+        'reply_markup': {
+            "inline_keyboard": [
+                [
+                    {"text": "google", "url": "http://www.google.com/"},
+                    {"text": "youtube", "url": "http://www.youtube.com/"}
+                ]
+            ]
+        }
+    }
+    requests.post(urls['sendMessage'], json=payload)
+
+def tel_send_button(chat_id):
+    payload = {
+        'chat_id': chat_id,
+        'text': "What is this?",    # button should be in the propper format as described
+        'reply_markup': {
+            'keyboard': [[
+                {
+                    'text': 'supa'
+                },
+                {
+                    'text': 'mario'
+                }
+            ]],
+        'one_time_keyboard' : True
+        }
+    }
+    r = requests.post(urls['sendMessage'], json=payload)
+    return r
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -241,15 +374,31 @@ def index():
                 tel_anime(chat_id)
             elif txt[:9] == '/addanime':
                 tel_add_anime(chat_id, txt)
+            elif txt == "/inline":
+                tel_send_inlinebutton(chat_id)
+            elif txt == "ic_A":
+                tel_send_message(chat_id, "You have clicked A")
+            elif txt == "ic_B":
+                tel_send_message(chat_id, "You have clicked B")
+            elif txt[:10] == "/inlineurl":
+                tel_send_inlineurl(chat_id)
+            elif txt == "/button":
+                tel_send_button(chat_id)
             else:
                 pass
                 
         except:
             traceback.print_exc()
+
+        try:
+            file_id = tel_parse_get_message(msg)
+            tel_upload_file(file_id)
+        except:
+            print("No file from index--->")
     
         return Response('ok', status=200)
     else:
         return "<h1>Welcome!</h1>"
  
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(threaded=True, debug=True)
